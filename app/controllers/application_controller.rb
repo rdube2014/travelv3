6e68d6eb74f516b910a9598ad4af9e9d05408787
Eric Snyder <ersnyder1@gmail.com>
2013-12-09 11:40:31 -0600

    Morning Updates Dec 9th

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+before_filter :configure_devise_params, if: :devise_controller?
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :home_city, :home_state, :home_country, :photo_url, :user_name, :email, :password, :password_confirmation)
+    end
+end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,9 @@ def full_name
   ([first_name, last_name] - ['']).compact.join(' ')                         
 end
 
+validates :first_name, :presence => true
+validates :last_name, :presence => true
+validates :user_name, :presence => true
+
+
 end
