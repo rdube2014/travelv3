c8e33d6a21d59cea42e1a9973d9684008bef9f16
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-12-09 11:18:34 -0600

    Removed Chicago default

--- db/migrate/20131121003543_create_users.rb
+++ db/migrate/20131121003543_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name 
       t.string :last_name
-      t.string :home_city, :default => "Chicago"
+      t.string :home_city
       t.string :home_state
       t.string :home_country
       t.string :user_name
